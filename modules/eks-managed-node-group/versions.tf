f1da81e23f5aa1a87e5fa1cdfe8c14eb51cd55c6
Jules Chendjou <julestl@yahoo.fr>
2023-10-09 13:41:12 -0400

    update aws version

--- modules/eks-managed-node-group/versions.tf
+++ modules/eks-managed-node-group/versions.tf
@@ -4,8 +4,8 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      # version = ">= 4.57"
-      version = ">= 3.63"
+      version = ">= 4.57"
+      
 
     }
   }
